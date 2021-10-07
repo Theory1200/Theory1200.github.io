9c62569fd6fd13925907bacdca702d9d6ee42085
Ghait <ghaitben2017@gmail.com>
2021-10-07 17:32:14 -0400

    NavBar can now be displayed on all pages | its html is present in main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    navbar = document.getElementById("NavBar")
+    navbar.innerHTML = `
+    <ul>
+        <img src="logo.png" alt="PlaceHolder for Logo" />
+        <li>link1</li>
+        <li>link2</li>
+        <li>link3</li>
+        <li>link4</li>
+    </ul> `
+}
+
