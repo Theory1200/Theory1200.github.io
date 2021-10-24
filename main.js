fb44188d5c957ef1813db56fe3970f0c1f74a2ce
Adham <aradw009@uottawa.ca>
2021-10-24 17:34:52 -0400

    Html and css fully function, Javascript needs more tinkering

--- main.js
+++ main.js
@@ -7,6 +7,27 @@ window.onload = () => {
         <a href="https://theory1200.github.io/about"><li class="item">About</li></a>
         <a href="https://theory1200.github.io/features"><li class="item">Features</li></a>
         <a href="https://theory1200.github.io/future"><li class="item">Future Plans</li></a>
-    </ul> `
+    </ul> `   
 }
 
+var $element = $('.each-event, .title');
+var $window = $(window);
+
+$window.on('scroll resize', check_for_fade);
+$window.trigger('scroll');
+
+function check_for_fade() {
+  var window_height = $window.height();
+  
+  $.each($element, function (event) {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_offset = $element.offset().top;
+    space = window_height - (element_height + element_offset - $(window).scrollTop());
+    if (space < 60) {
+      $element.addClass('non-focus');
+    } else {
+      $element.removeClass('non-focus');
+    }
+  });
+};
