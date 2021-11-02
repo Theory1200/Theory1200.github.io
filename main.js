4e65b42c844659f8f386cb26c0d04c9514f01a02
Ghait <ghaitben2017@gmail.com>
2021-11-02 17:09:55 -0400

    fixed position of Home on navBar

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ window.onload = () => {
     navbar.innerHTML = `
     <ul>
         <img class="logo" src="logo.jpeg" alt="PlaceHolder for Logo" />
-        <a href="https://theory1200.github.io/home"><li class="item">Home</li></a>
         <a href="https://theory1200.github.io/about"><li class="item">About</li></a>
         <a href="https://theory1200.github.io/features"><li class="item">Features</li></a>
         <a href="https://theory1200.github.io/future"><li class="item">Future Plans</li></a>
+        <a href="https://theory1200.github.io/home"><li class="item">Home</li></a>
     </ul> `   
 }
 
