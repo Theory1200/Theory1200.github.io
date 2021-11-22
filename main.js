7a91a7128f952d9d17e761b764199c696708b044
Olivier Guindon <oguindon@icloud.com>
2021-11-21 21:31:40 -0500

    changed rp logo and fixed issue of dissapearing voicey logo

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.onload = () => {
     navbar = document.getElementById("NavBar")
     navbar.innerHTML = `
     <ul>
-        <img class="logo" src="logo.jpeg" alt="PlaceHolder for Logo" />
+        <img class="logo" src="logo.png" alt="PlaceHolder for Logo" />
         <a href="https://theory1200.github.io/about"><li class="item">About</li></a>
         <a href="https://theory1200.github.io/features"><li class="item">Features</li></a>
         <a href="https://theory1200.github.io/future"><li class="item">Future Plans</li></a>
